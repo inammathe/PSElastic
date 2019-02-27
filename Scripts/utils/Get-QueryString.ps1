9aaf61a316d516062578cd49700c3c4f03713481
inammathe <inammathe@gmail.com>
2019-02-27 17:57:59 +1000

    fixed and simplified queryvars in query string

--- Scripts/utils/Get-QueryString.ps1
+++ Scripts/utils/Get-QueryString.ps1
@@ -2,8 +2,10 @@ function Get-QueryString {
     param (
         [PSCustomObject]$QueryVariables
     )
-    $variables = foreach ($variable in $QueryVariables.PSObject.Properties) {
-        "$($variable.Name.ToLower())=$($variable.Value)"
+    $variables = foreach ($variable in $QueryVariables | Get-Member -MemberType NoteProperty) {
+        $name = $variable.Name.ToLower()
+        $value = $QueryVariables."$($variable.Name)"
+        "$name=$value"
     }
     if ($variables) {
         Write-Output ('?' + (Join-Parts -Separator '&' -Parts $variables))
